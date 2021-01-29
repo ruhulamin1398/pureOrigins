529479b742798abe8afaf6ebae74a86401412751
sourovahmad <mdsourovahmad19@gmail.com>
2021-01-30 00:55:37 +0600

    footer modify

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,71 +1,79 @@
-<div class="st_section st_footer_section">
-    <div class="container pb-4">
-        <div class="row">
-            <div class="col-lg-12">
-
-                <div class="footer_inner_bg">
-                    <!-- row inner start -->
-                    <div class="row">
-                        <div class="col-sm-12 home_footer">
-                            <div class="st_footer_widget m-4 mb-4 pb-2">
-                                <!-- row second start -->
-                                <div class="row pt-4">
-
-                                    <div class="col-12 col-md-3">
-                                        <div class="widget widget_links">
-                                            <h4 class="widget_title">Our Company</h4>
-                                            <ul>
-                                                <li><a href="#">Pure Origins</a></li>
-
-
-                                                <li><a href="#">team@pureorigins.store</a></li>
-                                                <li><a href="#">www.pureorigins.store</a></li>
-                                                <li><a href="#">www.pureharvest.store</a></li>
-                                                <li><a href="#">Company registration: 10489945</a></li>
-                                             
-                                                <li><a href="#">VAT: 300138663</a> </li>
-
-                                            </ul>
-                                          
-                                        </div>
-                                    </div>
-
-                                    <div class="col-12 col-md-3">
-                                        <div class="widget widget_links">
-                                            <h4 class="widget_title">contact us</h4>
-                                            <ul>
-                                                <li><a href="#">Pure Origins</a></li>
-                                                <li><a href="#">13 Amsterdam Road</a></li>
-                                                <li><a href="#">Isle of Dogs</a></li>
-                                                <li><a href="#">London E14 3UU</a></li>
-                                            </ul>
-                                            <br> .
-                                            <ul>
-                                                <li><a href="#">44 Alaol Avenue </a></li>
-                                                <li><a href="#">Uttara, Sector 6</a></li>
-                                                <li><a href="#">Dhaka, Bangladesh </a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <div class="col-12 col-md-3">
-                                        <div class="widget widget_links">
-                                            <img src="https://i1.wp.com/pureorigins.store/wp-content/uploads/2020/03/logos1-1.png?fit=255%2C172&ssl=1"
-                                                alt="">
-                                        </div>
-                                    </div>
-                                    <div class="col-12 col-md-3">
-                                        <div class="widget widget_links">
-                                            <img src="https://i0.wp.com/pureorigins.store/wp-content/uploads/2020/08/118089973_347492573085770_4821180475926318345_n.png?fit=255%2C172&ssl=1"
-                                                alt="">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div><!-- row inner end -->
-                    </div>
+<footer class="footer-1">
+    <svg class="bg-decor bg-decor-4" width="1746" height="688" viewbox="0 0 1746 688" fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+        d="M1746 284.024C1746 404.483 1631.36 528.191 1485.29 581.431C1321.14 641.265 900.906 697.36 715.132 686.686C592.648 679.649 121.385 557.025 23.0317 338.904C-68.198 136.581 115.743 0 501.225 0C886.707 0 1746 28.186 1746 284.024Z"
+        fill="#FFEEF2"></path>
+</svg>
+    <div class="footer-1-top">
+        <div class="container-wide">
+            <div class="row row-30">
+                <div class="col-sm-6 col-lg-3">
+                    <p class="footer-title">Our Company</p>
+                    <ul class="footer-list">
+                        <li><a href="#">Pure Origins</a></li>
+                        <li><a href="#">team@pureorigins.store</a></li>
+                        <li><a href="#">www.pureorigins.store</a></li>
+                        <li><a href="#">www.pureharvest.store</a></li>
+                        <li><a href="#">Company registration: 10489945</a></li>
+                        <li><a href="#">VAT: 300138663</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <p class="footer-title">contact us</p>
+                    <ul class="footer-list">
+                        <li><a href="#">Pure Origins</a> </li>
+                        <li>Isle of Dogs</li>
+                        <li>London E14 3UU</li>
+                        <li></li>
+                        <li>44 Alaol Avenue </li>
+                        <li>Uttara, Sector 6</li>
+                        <li>Dhaka, Bangladesh </li>
+                    </ul>
+                </div>
+                <br>
+         
+                <div class="col-sm-6 col-lg-3">
+                    <p class="footer-title"></p>
+                    <ul class="footer-list">
+                        <img src="https://i1.wp.com/pureorigins.store/wp-content/uploads/2020/03/logos1-1.png?fit=255%2C172&ssl=1"
+                            alt="Pure Origin logo">
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <p class="footer-title"></p>
+                    <ul class="footer-list">
+                        <img src="https://i0.wp.com/pureorigins.store/wp-content/uploads/2020/08/118089973_347492573085770_4821180475926318345_n.png?fit=255%2C172&ssl=1"
+                            alt="Pure Origin logo">
+                    </ul>
                 </div>
             </div>
-        </div><!-- row -->
-    </div><!-- container -->
-</div><!-- container -->
+        </div>
+    </div>
+    <div class="footer-1-bottom text-center text-lg-left">
+        <div class="container-wide">
+            <div class="row row-15 align-items-center">
+                <div class="col-lg-6">
+                    <ul class="list-inline footer-1-bottom-list">
+                        <li class="list-inline-item"><a href="index.html"><img class="footer-1-logo"
+                                    src="images/logo-default-149x49.svg" alt="" width="149" height="49"></a></li>
+                        <li class="list-inline-item">
+                            <p class="rights"><span>&copy;&nbsp;</span><span
+                                    class="copyright-year"></span><span>&nbsp;</span><span>Roxxe</span></p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-6 text-lg-right">
+                    <ul class="list-inline group-sm">
+                        <li class="list-inline-item"><a class="icon icon-gray icon-md fa-facebook-f" href="#"></a></li>
+                        <li class="list-inline-item"><a class="icon icon-gray icon-md fa-instagram" href="#"></a></li>
+                        <li class="list-inline-item"><a class="icon icon-gray icon-md fa-twitter" href="#"></a></li>
+                        <li class="list-inline-item"><a class="icon icon-gray icon-md fa-pinterest" href="#"></a></li>
+                        <li class="list-inline-item"><a class="icon icon-gray icon-md fa-youtube-play" href="#"></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
