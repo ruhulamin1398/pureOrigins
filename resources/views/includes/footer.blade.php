26885e03e4a8ce1f8aac67f0f8482ac96052968a
sourovahmad <mdsourovahmad19@gmail.com>
2021-01-30 00:27:26 +0600

    footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,71 @@
+<div class="st_section st_footer_section">
+    <div class="container pb-4">
+        <div class="row">
+            <div class="col-lg-12">
+
+                <div class="footer_inner_bg">
+                    <!-- row inner start -->
+                    <div class="row">
+                        <div class="col-sm-12 home_footer">
+                            <div class="st_footer_widget m-4 mb-4 pb-2">
+                                <!-- row second start -->
+                                <div class="row pt-4">
+
+                                    <div class="col-12 col-md-3">
+                                        <div class="widget widget_links">
+                                            <h4 class="widget_title">Our Company</h4>
+                                            <ul>
+                                                <li><a href="#">Pure Origins</a></li>
+
+
+                                                <li><a href="#">team@pureorigins.store</a></li>
+                                                <li><a href="#">www.pureorigins.store</a></li>
+                                                <li><a href="#">www.pureharvest.store</a></li>
+                                                <li><a href="#">Company registration: 10489945</a></li>
+                                             
+                                                <li><a href="#">VAT: 300138663</a> </li>
+
+                                            </ul>
+                                          
+                                        </div>
+                                    </div>
+
+                                    <div class="col-12 col-md-3">
+                                        <div class="widget widget_links">
+                                            <h4 class="widget_title">contact us</h4>
+                                            <ul>
+                                                <li><a href="#">Pure Origins</a></li>
+                                                <li><a href="#">13 Amsterdam Road</a></li>
+                                                <li><a href="#">Isle of Dogs</a></li>
+                                                <li><a href="#">London E14 3UU</a></li>
+                                            </ul>
+                                            <br> .
+                                            <ul>
+                                                <li><a href="#">44 Alaol Avenue </a></li>
+                                                <li><a href="#">Uttara, Sector 6</a></li>
+                                                <li><a href="#">Dhaka, Bangladesh </a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-md-3">
+                                        <div class="widget widget_links">
+                                            <img src="https://i1.wp.com/pureorigins.store/wp-content/uploads/2020/03/logos1-1.png?fit=255%2C172&ssl=1"
+                                                alt="">
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-md-3">
+                                        <div class="widget widget_links">
+                                            <img src="https://i0.wp.com/pureorigins.store/wp-content/uploads/2020/08/118089973_347492573085770_4821180475926318345_n.png?fit=255%2C172&ssl=1"
+                                                alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div><!-- row inner end -->
+                    </div>
+                </div>
+            </div>
+        </div><!-- row -->
+    </div><!-- container -->
+</div><!-- container -->
